87ed05bfd93c0b7066ec4e4f2de622d8fc8cda20
Chris Birk <cmbirk@gmail.com>
2022-06-28 05:41:17 -0500

    build ncPolicyWatch scraper

--- src/functions/newsScraper.ts
+++ src/functions/newsScraper.ts
@@ -2,12 +2,15 @@ import { scraper as newsAndObserverScraper } from '../siteScrapers/newsAndObserv
 import { scraper as carolinaJournalScraper } from '../siteScrapers/carolinaJournal.js'
 import { scraper as businessNCScraper } from '../siteScrapers/businessNC.js'
 import { scraper as triangleBusinessJournalScraper } from '../siteScrapers/triangleBusinessJournal.js'
+import { scraper as ncPolicyWatchScraper } from '../siteScrapers/ncPolicyWatch.js'
 
 export const newsScraper = async () => {
-  const newsAndObserverLinks = await newsAndObserverScraper()
-  const carolinaJournalLinks = await carolinaJournalScraper()
-  const businessNCLinks = await businessNCScraper()
-  const triangleBusinessJournalLinks = await triangleBusinessJournalScraper()
+  // const newsAndObserverLinks = await newsAndObserverScraper()
+  // const carolinaJournalLinks = await carolinaJournalScraper()
+  // const businessNCLinks = await businessNCScraper()
+  // const triangleBusinessJournalLinks = await triangleBusinessJournalScraper()
+  const ncPolicyWatchLinks = await ncPolicyWatchScraper()
 
-  return { businessNCLinks, carolinaJournalLinks, triangleBusinessJournalLinks, newsAndObserverLinks }
+  // return { businessNCLinks, carolinaJournalLinks, triangleBusinessJournalLinks, newsAndObserverLinks }
+  return { ncPolicyWatchLinks }
 }
--- src/siteScrapers/ncPolicyWatch.ts
+++ src/siteScrapers/ncPolicyWatch.ts
@@ -1,18 +1,34 @@
 import puppeteer from 'puppeteer'
 
-const triangleBusinessJournalUrl = ''
+const ncPolicyWatchUrl = 'https://ncpolicywatch.com/category/articles/news/'
 
 export const scraper = async () => {
   const browser = await puppeteer.launch({ args: ['--no-sandbox'] })
   const page = await browser.newPage()
-  await page.goto(triangleBusinessJournalUrl)
+  await page.goto(ncPolicyWatchUrl)
 
   await page.setViewport({
     width: 1200,
     height: 800,
   })
 
-  console.log(`Getting ${triangleBusinessJournalUrl}`)
+  console.log(`Getting ${ncPolicyWatchUrl}`)
 
-  const listHandle = await page.$('')
+  const listHandle = await page.$('.all-layout')
+
+  if (listHandle) {
+    const articlesHandle = await listHandle.$$('[role=article]')
+    const articlesPromises = articlesHandle.map(async (article) => {
+      const title: string = await article.$eval('.entry-title > a', (el) => el.innerText)
+      const link: string = await article.$eval('.entry-title > a', (el) => el.getAttribute('href'))
+      return { title, link }
+    })
+
+    const links = await Promise.all(articlesPromises)
+
+    await browser.close()
+    return links
+  } else {
+    console.error(`Unable to get ${ncPolicyWatchUrl} news articles.`)
+  }
 }
