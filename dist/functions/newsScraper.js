7feec62199000ecc8a2d04040ff32a39fd463a1c
Chris Birk <cmbirk@gmail.com>
2022-06-28 05:47:03 -0500

    combine functions

--- dist/functions/newsScraper.js
+++ dist/functions/newsScraper.js
@@ -1,6 +1,20 @@
+import { scraper as newsAndObserverScraper } from '../siteScrapers/newsAndObserver.js';
+import { scraper as carolinaJournalScraper } from '../siteScrapers/carolinaJournal.js';
+import { scraper as businessNCScraper } from '../siteScrapers/businessNC.js';
+import { scraper as triangleBusinessJournalScraper } from '../siteScrapers/triangleBusinessJournal.js';
 import { scraper as ncPolicyWatchScraper } from '../siteScrapers/ncPolicyWatch.js';
 export const newsScraper = async () => {
+    const businessNCLinks = await businessNCScraper();
+    const carolinaJournalLinks = await carolinaJournalScraper();
+    const newsAndObserverLinks = await newsAndObserverScraper();
     const ncPolicyWatchLinks = await ncPolicyWatchScraper();
-    return { ncPolicyWatchLinks };
+    const triangleBusinessJournalLinks = await triangleBusinessJournalScraper();
+    return {
+        businessNCLinks,
+        carolinaJournalLinks,
+        ncPolicyWatchLinks,
+        newsAndObserverLinks,
+        triangleBusinessJournalLinks,
+    };
 };
 //# sourceMappingURL=newsScraper.js.map
