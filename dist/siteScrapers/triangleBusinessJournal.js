90989e2eb4ab18af56f4c3dfc8309dc70691a4df
Chris Birk <cmbirk@gmail.com>
2022-08-11 18:15:07 -0500

    wrap scrapers in try/catch block in case one fails. Return empty array in that case

--- dist/siteScrapers/triangleBusinessJournal.js
+++ dist/siteScrapers/triangleBusinessJournal.js
@@ -2,34 +2,40 @@ import puppeteer from 'puppeteer';
 const triangleBusinessJournalUrl = 'https://www.bizjournals.com/triangle/news/';
 const baseUrl = 'https://www.bizjournals.com/';
 export const scraper = async () => {
-    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
-    const page = await browser.newPage();
-    page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:101.0) Gecko/20100101 Firefox/101.0');
-    await page.goto(triangleBusinessJournalUrl);
-    await page.setViewport({
-        width: 1200,
-        height: 800,
-    });
-    console.log(`Getting ${triangleBusinessJournalUrl}`);
-    const listHandle = await page.$('.primary > .item--container');
-    if (listHandle) {
-        const articlesHandle = await listHandle.$$('a');
-        const articlesPromises = articlesHandle.map(async (article) => {
-            const link = `${baseUrl}${(await article.evaluate((el) => el.getAttribute('href')))}`;
-            const title = await article.$eval('.item__title', (el) => el.innerText);
-            return {
-                link,
-                title,
-            };
+    try {
+        const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+        const page = await browser.newPage();
+        page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:101.0) Gecko/20100101 Firefox/101.0');
+        await page.goto(triangleBusinessJournalUrl);
+        await page.setViewport({
+            width: 1200,
+            height: 800,
         });
-        const links = await Promise.all(articlesPromises);
+        console.log(`Getting ${triangleBusinessJournalUrl}`);
+        const listHandle = await page.$('.primary > .item--container');
+        if (listHandle) {
+            const articlesHandle = await listHandle.$$('a');
+            const articlesPromises = articlesHandle.map(async (article) => {
+                const link = `${baseUrl}${(await article.evaluate((el) => el.getAttribute('href')))}`;
+                const title = await article.$eval('.item__title', (el) => el.innerText);
+                return {
+                    link,
+                    title,
+                };
+            });
+            const links = await Promise.all(articlesPromises);
+            await browser.close();
+            return links;
+        }
+        else {
+            console.error(`Unable to get ${triangleBusinessJournalUrl} news articles.`);
+        }
         await browser.close();
-        return links;
+        return [];
     }
-    else {
-        console.error(`Unable to get ${triangleBusinessJournalUrl} news articles.`);
+    catch (err) {
+        console.error(err);
+        return [];
     }
-    await browser.close();
-    return [];
 };
 //# sourceMappingURL=triangleBusinessJournal.js.map
