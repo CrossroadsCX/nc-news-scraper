c75d77a6092a1d61a42c217a85eb95545b9a118c
Chris Birk <cmbirk@gmail.com>
2022-08-17 21:47:10 -0500

    build functions

--- dist/functions/newsScraper.d.ts
+++ dist/functions/newsScraper.d.ts
@@ -1,7 +1,4 @@
 export declare const newsScraper: () => Promise<{
     body: import("@sendinblue/client").CreateSmtpEmail;
     statusCode: number | undefined;
-} | {
-    body: unknown;
-    statusCode: number;
 }>;
