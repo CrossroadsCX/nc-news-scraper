35b73a54748b94ba861bda47ef544c05dd1dfe21
Chris Birk <cmbirk@gmail.com>
2022-08-17 21:47:10 -0500

    build functions

--- dist/sendinblue/sendEmail.d.ts
+++ dist/sendinblue/sendEmail.d.ts
@@ -3,7 +3,4 @@ import type { ArticleList } from '../types';
 export declare const sendEmail: (articles: ArticleList) => Promise<{
     body: SIBApi.CreateSmtpEmail;
     statusCode: number | undefined;
-} | {
-    body: unknown;
-    statusCode: number;
 }>;
