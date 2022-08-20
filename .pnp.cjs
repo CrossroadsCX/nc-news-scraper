c611f43527a74f30fa9d1db8e0c358206556e69e
Chris Birk <cmbirk@gmail.com>
2022-08-20 15:37:22 -0500

    finish campaign-style api calls for wotd emails

--- .pnp.cjs
+++ .pnp.cjs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e0f9341c8f39f7b6d94b62354f36a8b3268ecb9b336b711f0c98714f51d5dbe8
-size 856152
+oid sha256:962300ae9d1a414847b71e943bd70d510f84f900aa208ba5589db49f111f7c1a
+size 856415
